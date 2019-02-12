93b27b14979f33d6bbc5c5824f41033bb2f106f0
Jalia Evans <irohdesign@outlook.com>
2019-02-11 21:09:48 -0500

    Updates

---
+++ precache-manifest.7bcef01215d16bfb8fb9613c604acf40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3571bcbd1c4e99cc0a93",
+    "url": "/clicking-game/static/js/runtime~main.3571bcbd.js"
+  },
+  {
+    "revision": "ab1fa949d675634672f3",
+    "url": "/clicking-game/static/js/main.ab1fa949.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/clicking-game/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "ab1fa949d675634672f3",
+    "url": "/clicking-game/static/css/main.9787b95d.chunk.css"
+  },
+  {
+    "revision": "bc173537e9ec405f1a803907e9e4a954",
+    "url": "/clicking-game/index.html"
+  }
+];
